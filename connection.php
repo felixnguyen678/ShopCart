002978c6d52e8479e64b24b4be4b27fb9404e9d4
Bean <nguyenquangphuc.vn.hue@gmail.com>
2021-06-25 18:19:54 +0700

    neo4j connection

--- connection.php
+++ connection.php
@@ -1,37 +1,47 @@
-
-
 <?php
 # connection.php
-require_once ('config.php');
-/*
-class MySQLConnection
+require_once "vendor/autoload.php";
+use MongoDB\Client;
+class Mongo
 {
     private static $instance = NULl;
-    public static function getInstance() {
-        if (!isset(self::$instance)) {
-            try {
-                self::$instance = new PDO('mysql:host=localhost;dbname=demo_mvc', 'bean', '12345');
-                self::$instance->exec("SET NAMES 'utf8'");
-            } catch (PDOException $ex) {
-                die($ex->getMessage());
-            }
+    public static function getInstance(){
+        if(!isset(self::$instance)){
+            $con = new Client("mongodb+srv://leminh1031999:minh10399@cluster0.dkkvd.mongodb.net/test");
+            self::$instance = $con->selectDatabase("ShopCart_db");
         }
         return self::$instance;
     }
 }
+$mongo = Mongo::getInstance();
 
-$mysql = MySQLConnection::getInstance();
-*/
 
-class MongoConnection{
-
-}
-class RedisConnection{
+class Redis{
 
 }
-class CassandraConnection{
+class Cassandra{
 
 }
-class Neo4JConnection{
 
+
+require_once "vendor/autoload.php";
+use Neoxygen\NeoClient\ClientBuilder;
+class Neo4J{
+    private static $instance = NULL;
+    public static function getInstace(){
+        if(!isset(self::$instance)){
+            self::$instance = ClientBuilder::create()
+                ->addConnection('default', 'http', 'localhost', 7474)
+                ->setAutoFormatResponse(true)
+                ->build();
+        }
+        return self::$instance;
+    }
 }
+$neo4J = Neo4J::getInstace();
+
+//$result = $client->sendCypherQuery("match (n) return n")->getResult();
+//print_r($result->getTableFormat());
+
+
+
