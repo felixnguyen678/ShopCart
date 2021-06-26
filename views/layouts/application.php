78f09bdfa4cf7ff7f1be8a17a9e09c43f50f27a9
Bean <nguyenquangphuc.vn.hue@gmail.com>
2021-06-26 19:34:17 +0700

    components in homeview

--- views/layouts/application.php
+++ views/layouts/application.php
@@ -1,14 +1,47 @@
 
 <!-- # views/layouts/application.php-->
-<DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <title>Demo PHP MVC</title>
-    </head>
-    <body>
-    <?= @$content ?>
-    </body>
-    </html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Shopping cart</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap styles -->
+    <link href="/assets/css/bootstrap.css" rel="stylesheet"/>
+    <!-- Customize styles -->
+    <link href="/assets/css/style.css" rel="stylesheet"/>
+    <!-- font awesome styles -->
+    <link href="/assets/font-awesome/css/font-awesome.css" rel="stylesheet"/>
+</head>
+<body>
+<!--Header-->
+<?php
+    require_once('header.php');
+?>
+<div class="container">
+
+    <!--Body Section-->
+    <div class="row">
+        <!--categories-->
+        <?php
+        require_once ('categories.php');
+        ?>
+
+        <!--products-->
+        <?php
+        require_once('products.php');
+        ?>
+        </div>
+</div>
+<!-- footer -->
+<?php
+    require_once ('footer.php');
+?>
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="assets/js/jquery.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/jquery.easing-1.3.min.js"></script>
+<script src="assets/js/jquery.scrollTo-1.4.3.1-min.js"></script>
+<script src="assets/js/shop.js"></script>
+</body>
+</html>
